aa7304325fa2da07ab5eea420387c41b6e83aff4
Brittany Ellich <brittanyellich@github.com>
2023-03-24 21:49:57 +0000

    Change devcontainer to have minimum requirements and add setup script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Refer to this issue for setup commands: https://github.com/github/IADB-education/issues/5#issuecomment-1424872233
+mkdir -p ~/data/osrm/
+cd ~/data/osrm
+wget https://download.geofabrik.de/south-america/brazil/sul-latest.osm.pbf
+
+# You can replace the url to any geofabrik sub-region download link
